848a755424f0ed68e3914f0b3a3efb1c27a09d8e
nameofname <nameofemail@gmail.com>
2016-03-16 19:20:50 -0400

    the thing is starting to render... with errors.

--- public/js/components/NotesContainer.js
+++ public/js/components/NotesContainer.js
@@ -4,40 +4,46 @@
 const React = require("react");
 const ReactRedux = require("react-redux");
 const NoteRow = require("./NoteRow");
+const notesStore = require('../stores/notesStore');
 const noteActions = require('../actions/noteActions');
 const { connect } = ReactRedux;
 
-
-const component = React.createElement({
+// TODO !!!!!!!! move this back into the right place. into flerp
+//{props.notes.map(note => {
+//    return (<NoteRow id={ note.id } text={ note.text } updateHandler={ store.plerp }></NoteRow>)
+//})}
+const component = React.createClass({
     propTypes : {
         notes : React.PropTypes.array,
         addHandler : React.PropTypes.function
     },
     render () {
-        const props = this.getProps();
+        //const props = this.getProps();
         return (
             <div className='flerp'>This is contents bitch
-                {props.notes.map(note => {
-                    return (<NoteRow id={ note.id } text={ note.text } updateHandler={ store.plerp }></NoteRow>)
-                })}
             </div>
         );
-
-        return (
-            <div className="col-md-9">
-                <DropdownButton title="Apply Action" id="bulk-action-dropdown">
-                    {this.props.actions.map((action) => {
-                        return (<MenuItem eventKey={action.key} key={action.key}>{action.displayText}</MenuItem>);
-                    })}
-                </DropdownButton>
-                <span className="items-selected text-info"><i>0 items selected</i></span>
-            </div>
-        )
     }
 });
 
 
-const mapStateToProps = (state, props) => {};
+
+
+
+
+// TODO !!!!!! implement :
+const addHandler = '';
+const updateHandler = '';
+const deleteHandler = '';
+
+const mapStateToProps = (state, props) => {
+    return {
+        notes : notesStore,
+        addHandler,
+        updateHandler,
+        deleteHandler
+    };
+};
 const mapDispatchToProps = {
     addNote : noteActions.addNote,
     updateNote : noteActions.updateNote,
--- public/js/theFile.js
+++ public/js/theFile.js
@@ -3,17 +3,33 @@
 // this is the entry file.
 // create the notes store
 // get the notes component and render that with the store of notes.
+const React = require("react");
 const ReactDOM = require("react-dom");
 const notesStore = require('./stores/notesStore');
 const NotesContainer = require('./components/NotesContainer');
 
+const Provider = React.createClass({
+    propTypes : { store : React.PropTypes.object },
+    childContextTypes : { store : React.PropTypes.object },
+    getChildContext () {
+        return { store : this.props.store };
+    },
+    render () {
+        return this.props.children;
+    }
+});
+
 const render = () => {
+    const container = document.querySelectorAll('#container')[0];
     console.log('and awayyyyyy we go!');
-    console.log(notesStore.getState());
-    ReactDOM.render(NotesContainer, notesStore.getState());
-    //ReactDOM.render(<NotesContainer notes={...notesStore.getState()} ></NotesContainer>)
+    ReactDOM.render(
+        <Provider store={notesStore}>
+            <NotesContainer></NotesContainer>
+        </Provider>
+        ,container);
 };
 
-
-notesStore.subscribe(render);
-render();
+window.onload = function () {
+    notesStore.subscribe(render);
+    render();
+};
