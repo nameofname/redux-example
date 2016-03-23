07e2c63b9681ab2e76f7a1ead721276a193a777f
nameofname <nameofemail@gmail.com>
2016-03-23 14:16:53 -0400

    fixed bugs with var global to module.

--- public/js/components/NoteRow.js
+++ public/js/components/NoteRow.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const React = require("react");
-let input;// use the ref attribute to assign the input to the current input value below
 
 /**
  * This is the note row component. Each note row displays the note with an optional input to update.
@@ -14,20 +13,18 @@ const component = React.createClass({
         updateNote : React.PropTypes.func.isRequired,
         deleteNote : React.PropTypes.func.isRequired
     },
-    updateNoteHandler (e) {
-        e.preventDefault();
+    updateNoteHandler (input) {
         const val = input.value.trim();
         if (!val) { return; }
         this.props.updateNote(this.props.id, val);
         input.value = '';
     },
-    deleteNoteHandler (e) {
-        debugger; // do we get the e argument here?
-        e.preventDefault();
+    deleteNoteHandler () {
         this.props.deleteNote(this.props.id);
     },
     render () {
         const props = this.props;
+        let input;// use the ref attribute to assign the input to the current input value below
 
         return (
             <div className='row well'>
@@ -36,8 +33,14 @@ const component = React.createClass({
                 <br />
                 <span>Update Note : </span>
                 <input ref={ node => {input = node} } type='text' placeholder={ props.text } />
-                <input type='submit' className='btn' value='Update Note' onClick={ this.updateNoteHandler } />
-                <a href='*' onClick={ this.deleteNoteHandler }>delete</a>
+                <input type='submit' className='btn' value='Update Note' onClick={ e => {
+                    e.preventDefault();
+                    this.updateNoteHandler(input);
+                } } />
+                <a href='*' onClick={ e => {
+                    e.preventDefault();
+                    this.deleteNoteHandler();
+                } }>delete</a>
             </div>
         );
     }
